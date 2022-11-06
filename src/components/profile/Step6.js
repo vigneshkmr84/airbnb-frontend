80004fe79ab0eaeda3e6e58eb5a1dc22c8a0e596
Vignesh <vigneshkmr84@gmail.com>
2022-11-06 06:27:48 -0600

    Multi file handling with title

--- src/components/profile/Step6.js
+++ src/components/profile/Step6.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 /* step6:
 picture */
-const Step6 = ({ formData, setFormData }) => {
+const Step6 = ({ formData, setFormData, propertyImages, setPropertyImages }) => {
 
     const fileFormats = 'image/*, .heic';
 
@@ -11,25 +11,6 @@ const Step6 = ({ formData, setFormData }) => {
         console.log(file);
         var base64 = await convertBase64(file)
         setFormData({ ...formData, img: base64.split("base64,")[1] });
-        // console.log(base64)
-
-        /* const files = event.target.files
-        console.log(files.length)
-        files.map(async (file) => {
-            console.log(file);
-            const base64 = await convertBase64(file)
-            setFormData({ ...formData, img: base64 });
-        }) */
-    }
-
-    const handleMultipleImgUpload = async (event) => {
-        const files = event.target.files
-        console.log(files.length)
-        files.map(async (file) => {
-            console.log(file);
-            const base64 = await convertBase64(file)
-            setFormData({ ...formData, img: base64 });
-        })
     }
 
     const convertBase64 = (file) => {
@@ -45,6 +26,31 @@ const Step6 = ({ formData, setFormData }) => {
         })
     }
 
+    const onClickAddButton = (e) => {
+        console.log('add');
+        if (propertyImages.length > 5) {
+            console.log(propertyImages);
+            window.alert('Max 5 images')
+        } else {
+            setPropertyImages(prevState => (
+                [...prevState, { title: "", image: "" }]
+            ))
+        }
+    }
+
+    const handleChange = (e, i) => {
+        console.log('changing...')
+        const { name, value } = e.target;
+        console.log(name, value)
+        let images = [...propertyImages];
+        images[i] = { ...images[i], [name]: value };
+        setPropertyImages(images)
+    }
+
+
+    const handleImageUpload = (e,i) =>{
+
+    }
     return (
         <div>
             <div className='row'>
@@ -66,10 +72,40 @@ const Step6 = ({ formData, setFormData }) => {
                 </div> */}
             </div>
 
-            {/* <div className='row'>
+            <div className='row'>
                 <h6>Other pictures</h6>
             </div>
-            <div className='row'>
+            {propertyImages?.map((el, index) => {
+                return (
+                    <div className='row' key={index}>
+                        <input className="form-control w-25"
+                            placeholder={`Title ${index + 1}`}
+                            type="text"
+                            name='title'
+                            value={el.title}
+                            required
+                            onChange={e => handleChange(e, index)}
+                        />
+                        <div className='col'>
+                            <input
+                                className="form-control"
+                                type='file'
+                                placeholder={`Upload image ${index + 1}`}
+                                accept={fileFormats}
+                                name='image'
+                                value={el.image}
+                                onChange={e => handleImageUpload(e, index)}
+                            ></input>
+                        </div>
+                    </div>
+                )
+            })}
+            {/* <div className='row'>
+                <input className="form-control w-25"
+                    placeholder='Image 1'
+                    type="text"
+                    required
+                />
                 <div className='col'>
                     <input
                         className="form-control"
@@ -79,12 +115,16 @@ const Step6 = ({ formData, setFormData }) => {
                         onChange={e => handleMultipleImgUpload(e)}
                     ></input>
                 </div>
-                <div className='col'>
-                    <button className='btn btn-danger'>
-                        <i className="bi bi-trash"></i>
-                    </button>
-                </div>
             </div> */}
+
+            <div className='row' style={{ justifyContent: 'center', textAlign: 'center' }}>
+                <button
+                    className='btn btn-primary w-25'
+                    style={{ alignItems: 'center' }}
+                    onClick={(e) => onClickAddButton(e)}>
+                    <i className="bi bi-plus-lg" /> &nbsp;Add
+                </button>
+            </div>
         </div>
     )
 }
