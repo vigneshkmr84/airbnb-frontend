6fce4be10d864a329b7df01c5c58b80e58f6ac2a
Vignesh <vigneshkmr84@gmail.com>
2022-12-10 05:47:50 -0600

    Welcome message in login page added

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -4,6 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import { callLoginApi } from '../../services/LoginService';
 import { useNavigate } from 'react-router-dom';
 import Signup from '../signup/Signup';
+// import logo from './logo/logo3.svg';
 
 
 const Login = () => {
@@ -40,52 +41,58 @@ const Login = () => {
 
     return (
 
-        <div className='container' id='login-container'>
-            <div className='row form-group' id='login-header'>
-                <h2 className="fw-bold mb-2" style={{ padding: 30 }}>Log in</h2>
+        <div style={{ display: 'inline-block', width: '100%' }}>
+            <div className='container'>
+                <h1 style={{ textAlign: 'center', paddingTop: '3%', font: '' }}>Welcome to Airdnd</h1>
+                <h6 style={{ textAlign: 'center' }}>Book your stays</h6>
             </div>
-            <div id='login-body' className='container'>
-                <div className='row form-group'>
-                    <label className='form-label'>Username</label>
-                    <input type="text"
-                        className='form-control form-control-md'
-                        placeholder='Email id / Phone no' required
-                        name="userName"
-                        onChange={handleInputChange}
-                    >
-                    </input>
+            <div className='container' id='login-container'>
+                <div className='row form-group' id='login-header'>
+                    <h2 className="fw-bold mb-2" style={{ padding: '1%' }}>Log in</h2>
+                </div>
+                <div id='login-body' className='container'>
+                    <div className='row form-group'>
+                        <label className='form-label'>Username</label>
+                        <input type="text"
+                            className='form-control form-control-md'
+                            placeholder='Email id / Phone no' required
+                            name="userName"
+                            onChange={handleInputChange}
+                        >
+                        </input>
+                    </div>
+
+                    <div className="row form-group">
+                        <label className='form-label'>Password </label>
+                        <input type="Password"
+                            className='form-control form-control-md'
+                            placeholder='Password' required
+                            name="password"
+                            onChange={handleInputChange}
+                        >
+                        </input>
+                    </div>
+
+                    <div className="row form-group" style={{ textAlign: 'center', justifyContent: 'center', paddingTop: '6%' }}>
+                        <button type="button"
+                            className="btn btn-primary btn-lg w-50"
+                            onClick={e => onClickLoginSubmit(e)}
+                            disabled={!isEnabled}
+                        > Login
+                        </button>
+                    </div>
                 </div>
 
-                <div className="row form-group">
-                    <label className='form-label'>Password </label>
-                    <input type="Password"
-                        className='form-control form-control-md'
-                        placeholder='Password' required
-                        name="password"
-                        onChange={handleInputChange}
-                    >
-                    </input>
+                <div id='login-footer'>
+                    <div className="row form-group" style={{ textAlign: 'center', paddingTop: '5%' }}>
+                        <p style={{ fontSize: '12px' }}>Don't have an account?&nbsp;
+                            <a className="text-primary" onClick={(e) => { setShowSignup(true) }}>Sign up</a>
+                        </p>
+                    </div>
                 </div>
 
-                <div className="row form-group" style={{ textAlign: 'center', justifyContent: 'center', paddingTop: '6%' }}>
-                    <button type="button"
-                        className="btn btn-primary btn-lg w-50"
-                        onClick={e => onClickLoginSubmit(e)}
-                        disabled={!isEnabled}
-                    > Login
-                    </button>
-                </div>
+                <Signup showSignup={showSignup} setShowSignup={setShowSignup} />
             </div>
-
-            <div id='login-footer'>
-                <div className="row form-group" style={{ textAlign: 'center', paddingTop: '5%' }}>
-                    <p style={{ fontSize: '12px' }}>Don't have an account?&nbsp;
-                        <a className="text-primary" onClick={(e) => { setShowSignup(true) }}>Sign up</a>
-                    </p>
-                </div>
-            </div>
-
-            <Signup showSignup={showSignup} setShowSignup={setShowSignup} />
         </div>
     )
 
